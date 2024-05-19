b500ed8d807750303ddc348bb8a5f55322c545aa
Samuel Briones Nava <brayan.briones@alumnos.udg.mx>
2024-05-19 14:56:19 -0600

    update driveFinal

--- roboclaw_node/src/driveFinal.py
+++ roboclaw_node/src/driveFinal.py
@@ -6,9 +6,9 @@ from roboclaw_3 import Roboclaw
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 
-global max_voltage, voltage_battery
-voltage_battery = 21
-max_voltage = 14
+global maxVoltage, voltageBattery
+voltageBattery = 21
+maxVoltage = 14
 def limit_speed(max, left, right):
     if left > max:
         left = max
@@ -20,9 +20,6 @@ def cmd_vel_callback(msg):
     global roboclaw_left_speed, roboclaw_right_speed, mode
     distancia_llantas = 0.8
     
-    #scale_linear_turbo: 0.7
-	#scale_angular_turbo: 0.6
-    
     roboclaw_right_speed = msg.linear.x + (distancia_llantas/2)*msg.angular.z
     roboclaw_left_speed = msg.linear.x - (distancia_llantas/2)*msg.angular.z
     if mode == 5:
@@ -31,15 +28,10 @@ def cmd_vel_callback(msg):
         max_speed = 1
     else:
         max_speed = 1
-    roboclaw_left_speed, roboclaw_right_speed, = limit_speed(max_speed, roboclaw_left_speed, roboclaw_right_speed)
+    roboclaw_left_speed, roboclaw_right_speed = limit_speed(max_speed, roboclaw_left_speed, roboclaw_right_speed)
     if (msg.angular.z == 2.5 or msg.angular.z == -2.5 or msg.angular.z == 1.5 or msg.angular.z == -1.5) and msg.linear.x == 0:
         roboclaw_right_speed *=2
         roboclaw_left_speed *=2
-
-    # print("Left: ",roboclaw_left_speed)
-    # print("Right: ",roboclaw_right_speed)
-    # rc.DutyAccelM1M2(address, accel1=655359, duty1=1, accel2=655359, duty2=1) # address, accel1 0 to 655359, duty1 -32768 to +32767. accel2 0 to 655359, duty2 -32768 to +32767.
-
 
 def joy_callback(msg):
     global mode
@@ -51,72 +43,80 @@ def joy_callback(msg):
     #     mode = "No hay ningun boton presionado"
     # print(mode)
 
-def main():
-    global roboclaw_left_speed, roboclaw_right_speed, mode
-    mode = 1
-    rospy.init_node("roboclaw_speed")
-    # print("Waiting for topic /joy_roboclaw")
-    print("Waiting for topic /cmd_vel")
-    # rospy.wait_for_message("/joy_roboclaw",Joy, timeout=50)
-    rospy.wait_for_message("/cmd_vel",Twist)
-    # rospy.Subscriber("/joy_roboclaw",Joy, joy_callback)
-    rospy.Subscriber("/cmd_vel", Twist, cmd_vel_callback)
-    loop = rospy.Rate(3)
+def reads(address,rcs):
+    for rc in rcs:
+        print("Battery: ", rc.ReadMainBatteryVoltaje(address))
+        print("Current M1: ", rc.ReadCurrents(address)[1])
+        print("Current M2: ", rc.ReadCurrents(address)[2])
 
+
+def openRcs():
+    rcs = []
+    for i in range(3):
+        rc = Roboclaw("/dev/ttyACM"+str(i), 115200)
+        rc.Open()
+        rcs.append(rc)
+    return rcs
+def permisos():
     system("sudo chmod 777 /dev/ttyACM0")
     system("sudo chmod 777 /dev/ttyACM1")
-    # system("sudo chmod 777 /dev/ttyACM2")
+    system("sudo chmod 777 /dev/ttyACM2")
 
-    rc1 = Roboclaw('/dev/ttyACM0', 115200)
-    rc2 = Roboclaw('/dev/ttyACM1', 115200)
-    # rc3 = Roboclaw('/dev/ttyACM2', 115200)
-
-    address = 0x80
-
-    rc1.Open()
-    rc2.Open()
-    # rc3.Open()
-    #address = 0x80
-    rcs = [rc1, rc2]
-
-    version1 = rc1.ReadVersion(address)
-    version2 = rc2.ReadVersion(address)
-    # version3 = rc3.rc3ReadVersion(address)
+def versions(rcs, address):
+    version1 = rcs[0].ReadVersion(address)
+    version2 = rcs[1].ReadVersion(address)
+    version3 = rcs[2].ReadVersion(address)
 
     if version1[0] == False:
         print("GETVERSION1 Failed")
     elif version2[0] == False:
         print("GETVERSION2 Failed")
-    # elif version3[0] == False:
-    #     print("GETVERSION3 Failed")
+    elif version3[0] == False:
+        print("GETVERSION3 Failed")
     else:
         print(repr(version1[1]))
         print(repr(version2[1]))
-        # print(repr(version3[1]))
+        print(repr(version3[1]))
 
+def meanVoltage(rcs, address):
+    prom_voltage = 0
+    for i in range(len(rcs)):
+        prom_voltage += rcs[i].ReadMainBatteryVoltage(address)[1]
+        return int(prom_voltage/len(rcs))
+def main():
+    global roboclaw_left_speed, roboclaw_right_speed, mode
+    permisos()
+    rospy.init_node("roboclaw_speed")
+    print("Waiting for topic /joy_roboclaw")
+    print("Waiting for topic /cmd_vel")
+    rospy.wait_for_message("/joy_roboclaw",Joy)
+    rospy.wait_for_message("/cmd_vel",Twist)
+    rospy.Subscriber("/joy_roboclaw",Joy, joy_callback)
+    rospy.Subscriber("/cmd_vel", Twist, cmd_vel_callback)
+    loop = rospy.Rate(3)
+
+    mode = 1
+    address = 0x80
+    rcs = openRcs()
+    versions(rcs,address)
     roboclaw_left_speed = 0
     roboclaw_right_speed = 0
+
     try:
         while not rospy.is_shutdown():
-            prom_voltage = 0
-            for i in range(len(rcs)):
-                prom_voltage += rcs[i].ReadMainBatteryVoltage(address)[1]
-                print(f"Battery {i+1} voltage: {rcs[i].ReadMainBatteryVoltage(address)}")
-            voltage_battery = int(prom_voltage/2)
-            print(f"Mean Voltage: {voltage_battery}")
             roboclaw_left_speed = roboclaw_left_speed/2
             roboclaw_right_speed = roboclaw_right_speed/2
-            dutyMax = (32767/voltage_battery)*max_voltage*10    
+            voltageBattery = meanVoltage(rcs,address)
+            dutyMax = (32767/voltageBattery)*maxVoltage*10
             dutyLeft = int(dutyMax * roboclaw_left_speed)
             dutyRight = int(dutyMax * roboclaw_right_speed)
-            print("Current: ",rc1.ReadCurrents(address))
             print("Left: ",roboclaw_left_speed)
             print("Right: ",roboclaw_right_speed)
-            print("Left: ",dutyLeft)
-            print("Right: ",dutyRight)
+            # print("Left: ",dutyLeft)
+            # print("Right: ",dutyRight)
             
-            for i in rcs:
-                i.DutyAccelM1M2(address, 200000, dutyRight, 200000, dutyLeft)
+            for rc in rcs:
+                rc.DutyAccelM1M2(address, 200000, dutyRight, 200000, dutyLeft)
             loop.sleep()
     except Exception:
         print("Detener", 0)
